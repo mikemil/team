1e1ba6f30b06f38626788fc333cd8260faeca78d
plk3715 <plk3715>
2016-07-28 22:35:09 -0500

    Initial commit

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('team')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
---
+++ src/app/index.route.js
@@ -0,0 +1,40 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('team')
+    .config(routeConfig);
+
+  function routeConfig($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainController',
+        controllerAs: 'main'
+      })
+      .when('/home', {
+        templateUrl: 'app/main/home.html',
+        controller: 'MainController',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'app/main/about.html',
+        controller: 'MainController',
+        controllerAs: 'main'
+      })
+      .when('/contact', {
+        templateUrl: 'app/main/contact.html',
+        controller: 'MainController',
+        controllerAs: 'main'
+      })
+      .when('/details', {
+        templateUrl: 'app/main/details.html',
+        controller: 'MainController',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }
+
+})();
