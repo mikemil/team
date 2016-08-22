27f0725e26e4204fdd700906cc0b686d55bb61cb
plk3715 <plk3715>
2016-08-21 23:21:38 -0500

    more event fixes

--- src/app/main/calendar.controller.js
+++ src/app/main/calendar.controller.js
@@ -9,18 +9,21 @@
 
     calendar.message = "Welcome to the website.  This is just a quick site put together to learn AngularJS 1.x";
     
-    $scope.dateClick = function(event) {
-        console.log('dateClick event received:'+event.title);
+    $scope.dateClick = function(date) {
+        // console.log('dateClick event received:'+event.title);
+        console.log('dateClick event triggered');
     }
 
-    $scope.eventClick = function(event) {
-        console.log(' '+event.title+' clicked');
+    $scope.eventClick = function(date) {
+        // console.log('eventClick: '+event.title);
+        console.log('eventClick triggered for '+date.event.title );
+        alert('Event: '+date.event.title);
     }
     
     $scope.calendarOptions = {
         defaultDate: "2016-08-19",
         minDate: new Date(),
-        maxDate: new Date([2020, 12, 31]),
+        maxDate: new Date([2017, 12, 31]),
         dayNamesLength: 3, // How to display weekdays (1 for "M", 2 for "Mo", 3 for "Mon"; 9 will show full day names; default is 1)
         multiEventDates: true, // Set the calendar to render multiple events in the same day or only one event, default is false
         maxEventsPerDay: 1, // Set how many events should the calendar display before showing the 'More Events' message, default is 3;
@@ -29,8 +32,12 @@
     };
 
     $scope.events = [
-      {title: 'WOGA Plano', date: new Date([2016, 8, 27])},
-      {title: 'WOGA Frisco', date: new Date([2016, 8, 28])}
+      {title: 'Golden Grips Classic', date: new Date([2016, 8, 27])},
+      {title: 'Golden Grips Classic2', date: new Date([2016, 8, 27])},
+      {title: 'Golden Grips Classic3', date: new Date([2016, 8, 27])},
+      {title: 'WOGA Frisco Qualifier', date: new Date([2016, 8, 28])},
+      {title: 'Level 3/4/5 Qualifier', date: new Date([2016, 9, 10])},
+      {title: 'North Texas State Meet', date: new Date([2016, 9, 24])},
     ];
     
   }
