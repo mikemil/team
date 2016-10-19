dc52d20c0c40be82d00d8902a10be12195934dcd
plk3715 <plk3715>
2016-10-18 23:26:06 -0500

    got the view scores working

--- src/app/main/scorescontroller.js
+++ src/app/main/scorescontroller.js
@@ -15,19 +15,39 @@
     scoresService.getScores(scores.meetId).then(function(dataResponse) {
       scores.meet = dataResponse.data;
       console.log('getScores() response:'+scores.meet);
+
+      homeService.getMembers().then(function (dataResponse) {
+          scores.members = dataResponse.data;
+          scores.addNameToScore();
+          //scores.createNameMap();
+      }) 
     })
 
-    homeService.getMembers().then(function (dataResponse) {
-      scores.members = dataResponse.data;
-      scores.createNameMap();
-    })
+    // homeService.getMembers().then(function (dataResponse) {
+    //   scores.members = dataResponse.data;
+    //   scores.createNameMap();
+    // })
    
+    scores.addNameToScore = function () {
+       for (var i = 0; i < scores.meet.members.length; i++) {
+        var id = scores.meet.members[i].id;
+        
+        for (var x = 0; x < scores.members.length; x++ ) {
+          if (id === scores.members[x].id) {
+              scores.meet.members[i].name = scores.members[x].firstName + ' ' + scores.members[x].lastName;
+              console.log('setting name: '+scores.meet.members[x])
+          }
+        }
+      }
+    }
+
+
     scores.createNameMap = function() {
       var arrayLength = scores.members.length;
       for (var i = 0; i < arrayLength; i++) {
-        var key = scores.members[i].id;
+        var key = scores.members[i].id.toString();
         var value = scores.members[i].firstName + ' ' + scores.members[i].lastName;
-        scores.nameMap.key = value;
+        scores.nameMap[key] = value;
       }
       console.log("nameMap: "+scores.nameMap);
     }
